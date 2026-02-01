8e69e334fde0a4bf2ac02533f089f90573a8fc35
Node Sans <node189@icloud.com>
2026-02-01 15:34:26 +0800

    feat: add support for js devlopment

--- README.md
+++ README.md
@@ -7,11 +7,33 @@ cargo install emacs-lsp-booster
 brew install gnu-tar # needed on mac
 brew install rg
 brew install fd
+
+# tools to install lsp
+brew install uv
+brew install bun
+```
+
+## Install lsp
+
+``` bash
+# python
+uv tool install ty
+uv tool install ruff
+
+# c++
+brew install clangd
+
+# rust
+rustup component add rust-analyzer
+
+# javascript
+bun add -g typescript-language-server
+bun add -g prettier
 ```
 
 ## Install emacs on mac os
 
-### emacs-mac (recommand)
+### emacs-mac
 
 ``` bash
 brew tap railwaycat/emacsmacport
@@ -20,7 +42,7 @@ brew pin emacs-mac # Freeze the version
 cp -a $(brew --prefix)/opt/emacs-mac/Emacs.app /Applications
 ```
 
-### emacs-plus
+### emacs-plus (recommand)
 
 ``` bash
 brew tap d12frosted/emacs-plus
---
+++ lisp/lang/init-web.el
@@ -0,0 +1,45 @@
+;;; init-web.el --- Web Development -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+;; JavaScript
+(use-package js
+  :straight nil
+  :mode ("\\.js\\'" . js-mode)
+  :custom
+  (js-indent-level 2)
+  :config
+  ;; Ensure buffers inherit the intended default.
+  (setq-default js-indent-level 2)
+  (with-eval-after-load 'apheleia
+    (setf (alist-get 'js-mode apheleia-mode-alist) 'prettier-javascript)
+    (setf (alist-get 'js-ts-mode apheleia-mode-alist) 'prettier-javascript)))
+
+;; JSON
+(use-package json-mode
+  :straight t
+  :mode "\\.json\\'"
+  :config
+  (with-eval-after-load 'apheleia
+    (setf (alist-get 'json-mode apheleia-mode-alist) 'prettier-json)
+    (setf (alist-get 'json-ts-mode apheleia-mode-alist) 'prettier-json)))
+
+;; Configure Prettier formatters
+(with-eval-after-load 'apheleia
+  ;; Add Prettier formatters
+  (setf (alist-get 'prettier-javascript apheleia-formatters)
+        '("prettier" "--stdin-filepath" filepath))
+  (setf (alist-get 'prettier-typescript apheleia-formatters)
+        '("prettier" "--stdin-filepath" filepath))
+  (setf (alist-get 'prettier-json apheleia-formatters)
+        '("prettier" "--stdin-filepath" filepath))
+  (setf (alist-get 'prettier-html apheleia-formatters)
+        '("prettier" "--stdin-filepath" filepath))
+  (setf (alist-get 'prettier-css apheleia-formatters)
+        '("prettier" "--stdin-filepath" filepath)))
+
+(provide 'init-web)
+;;; init-web.el ends here
